4df782c6f9e167ccc63bc2e5da44aad043174a8e
MU94W <nanqiao15@126.com>
2018-09-05 00:49:22 +0800

    mv declaration

--- bin/mlpg/_mlpgGO.cc
+++ bin/mlpg/_mlpgGO.cc
@@ -6,7 +6,11 @@
 #include "lib/common.hpp"
 //#include "mkl.h"
 //#include "cblas.h"
-#include "mlpgGO.hpp"
+
+void build_poe(const float *b, const float *d_b, const float *dd_b, float *ret_b,
+               const float *tau, const float *d_tau, const float *dd_tau, float *ret_prec, const int n);
+
+void pentaband_linearsolve(float *A, float *b, float *x, const int n);
 
 FrameSeq *mlpgGO(FrameSeq *features, FrameSeq *covariance, const int static_dimension)
 /*
--- bin/mlpg/mlpgGO.hpp
+++ bin/mlpg/mlpgGO.hpp
@@ -1,6 +1,1 @@
 FrameSeq *mlpgGO(FrameSeq *features, FrameSeq *covariance, const int static_dimension);
-
-void build_poe(const float *b, const float *d_b, const float *dd_b, float *ret_b,
-               const float *tau, const float *d_tau, const float *dd_tau, float *ret_prec, const int n);
-
-void pentaband_linearsolve(float *A, float *b, float *x, const int n);
